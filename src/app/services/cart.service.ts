5ca7859874f7a59a73d414282ac8db5ecfe8ceb8
James <tujames77@gmail.com>
2025-09-03 00:05:36 +0700

    c

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -24,8 +24,45 @@ export class CartService {
     this.cart.items.push(new CartItem(food));
   }
 
-// Remove from cart
+// Remove from cart 
   removeFromeCart(foodId: string):void{
-    
+    this.cart.items = this.cart.items.filter(item => item.food.id !== foodId);
+  }
+
+// Change quantity
+  changeQuantity(foodId: string, quantity: number){
+    let cartItem = this.cart.items
+    .find(item => item.food.id === foodId)
+    // If cart item not found then return
+    if(!cartItem) return;
+
+    cartItem.quantity = quantity;
+    // Calculate the total price
+    cartItem.price = Number(quantity) * Number(cartItem.food.price);
+  }
+// Clear the cart
+  clearCart(){
+    this.cart = new Cart();
+  }
+// Get the cart observable
+  getCartObservable(){
+    // asObservable = cartSubject is an object send value to outside we want to use it inside the cart
+    return this.cartSubject.asObservable();
+  }
+
+  private setCartToLocalStorage():void{
+    this.cart.totalprice = this.cart.items.reduce((prevSum, currentItem)=> prevSum + currentItem.price,0);
+    this.cart.totalcount = this.cart.items.reduce((prevSum, currentItem)=> prevSum + currentItem.quantity,0);
+
+    // string presentation of the cart
+    const cartJson = JSON.stringify(this.cart);
+    // set the cart to the local storage
+    localStorage.setItem('Cart', cartJson);
+    this.cartSubject.next(this.cart); // we want to notify all the subscriber that the cart has been updated
+  }
+  
+  private getCartFromLocalStorage():Cart{
+    const cartJson = localStorage.getItem('Cart');
+    return cartJson? JSON.parse(cartJson): new Cart();
   }
 }
